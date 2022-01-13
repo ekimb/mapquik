83de8bf1b3c2832c26c7066c16e3f48d941c97ce
Rayan  CHIKHI <rchikhi@maestro-submit.maestro.pasteur.fr>
2022-01-13 15:13:11 +0100

    some cleanup

---
+++ example/run_ecoli_100k.sh
@@ -0,0 +1,1 @@
+cargo run --release -- nearperfect-ecoli.10X.100k.fa --reference ecoli.genome.100k.fa -k 6 -d 0.1 -l 22 -p hifimap --threads 11 --fmin 2
---
+++ example/run_ecoli_full.sh
@@ -0,0 +1,6 @@
+cargo run --release -- nearperfect-ecoli.10X.fa --reference ecoli.genome.fa -k 6 -d 0.1 -l 22 -p hifimap --threads 11 --fmin 2
+paftools.js mapeval hifimap.paf
+
+# minimap2
+minimap2 -x map-hifi ecoli.genome.fa nearperfect-ecoli.10X.fa > minimap2.paf
+paftools.js mapeval minimap2.paf
