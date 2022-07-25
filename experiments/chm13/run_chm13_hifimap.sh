1053f02bec19fa6cd37f50486f8054746c7e2938
Baris Ekim <ekimb@mit.edu>
2022-07-25 14:09:04 -0400

    change num threads in chm13 scripts

--- experiments/chm13/run_chm13_hifimap.sh
+++ experiments/chm13/run_chm13_hifimap.sh
@@ -5,5 +5,5 @@ G=$4
 echo "hifimap -------------"
 
 
-/usr/bin/time cargo run --release -- nearperfect-chm13.10X.fa --reference ecoli.genome.fa -k $K -l $L -d $D -g $G -p hifimap-$K-$L-$D-$G --threads 8 > hifimap-$K-$L-$D-$G.out
+/usr/bin/time cargo run --release -- nearperfect-chm13.10X.fa --reference ecoli.genome.fa -k $K -l $L -d $D -g $G -p hifimap-$K-$L-$D-$G --threads 48 > hifimap-$K-$L-$D-$G.out
 paftools.js mapeval hifimap-$K-$L-$D-$G.paf
